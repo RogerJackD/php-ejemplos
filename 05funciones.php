6ca5330b9b44c9e472503856da123648faf1bfab
Franklin <franklinramos061@gmail.com>
2024-05-15 17:04:19 -0500

    se agregó funciones de calcular suma rest, etc

--- 05funciones.php
+++ 05funciones.php
@@ -1,10 +1,44 @@
 <?php
-function obtenersaludo($parametro){
-    return "hola como estas ".$parametro;
+
+
+
+function obtenerSaludo($parametro) {
+
+  return "Hola como estas ".$parametro;
+
 }
 
 
 
+function mostrarDatos($nombres,$apellidos,$curso,$semestre){
 
-echo obtenersaludo("roger")
+  $mensaje = "Hola soy ".$nombres." ".$apellidos
+
+  ." y estoy en el curso ".$curso." del ".$semestre;
+
+  return $mensaje;
+
+}
+function calcular($operador,$num1,$num2){
+    if($operador=="+")
+        return $num1+$num2;
+    if($operador=="-")
+        return $num1-$num2;
+    if($operador=="*")
+        return $num1*$num2;
+    if($operador=="/")
+        return $num1/$num2;
+    
+    
+
+}
+
+
+
+echo obtenerSaludo("roger")."\n";
+
+echo mostrarDatos("roger","jack",
+
+"backend developer web","tercer semestre")."\n";
+echo "el total de la suma es:" .calcular("/",0,22);
 ?>
