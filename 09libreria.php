9995a5d97a305de11fddc239b6f7d600e16ff015
Franklin <franklinramos061@gmail.com>
2024-05-16 17:41:52 -0500

    nuevos codigos

---
+++ 09libreria.php
@@ -0,0 +1,24 @@
+<?php
+    function sumar($num1, $num2){
+        return $num1 + $num2;
+    }
+
+    function restar($num1, $num2){
+        return $num1 - $num2;
+    }
+
+    function multi($num1, $num2){
+        return $num1 * $num2;
+    }
+
+    function divi($num1, $num2){
+        if($num2==0) return "no divisible por 0";
+        return $num1 / $num2;
+    }
+    echo"ingresa los numeros";
+
+    
+
+
+    
+?>
---
+++ 10boolean.php
@@ -0,0 +1,28 @@
+<?php
+    $luzencendido=false;
+
+    if($luzencendido){
+        echo "luz esta apagada";
+    }
+    else{
+    
+    echo"la luz esta apagada";
+
+    $sintomas_covid =[
+        "tos" => true,
+        "fiebre" => false,
+        "dolor" => false,
+        "perdida olfato" => false
+    ];
+}
+
+//caso 03
+
+    if($sintomas_covid)["tos"]&&
+        $sintomas_covid["fiebre"]{
+        echo "estas enfermo tienes covid";
+    }
+    else{
+        echo "aun no tienes covid";
+    }
+?>
