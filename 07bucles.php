9726c45b045b11d452e776a91d4df5381f0955e2
RogerJackD <1511638@senati.pe>
2024-05-16 11:53:36 -0500

    se agregó logica de dar MENOR número

--- 07bucles.php
+++ 07bucles.php
@@ -1,5 +1,4 @@
 <?php
-
 $contador = 1;
 $sumanota= 0;
 while ($contador<=3){
@@ -10,16 +9,30 @@ while ($contador<=3){
     $contador++;
 
 }
-$notapromedio = $sumanotas/3;
-echo "\n el promedio de n0tas es: ".$notapromedio;
+$notapromedio = $sumanota/3;
+echo "\n el promedio de n0tas es: ".$notapromedio."\n";
 
 //logica permiter dar el mayor numero de 5 numeros
-$mayor = 0
-for($i=1,$i<=5; $i++) {
-    echo "ingrese su numero: ";
+$mayor = 0;
+for ($i = 1; $i <= 5; $i++) {
+    echo "Ingrese su numero: ";
     $numero = fgets(STDIN);
-    if($mayor<$numero){
-        $mayor=$numero;
+    if ($mayor < $numero) {
+        $mayor = $numero;
     }
 }
+echo "El mayor numero es: " . $mayor;
+
+//logica permiter dar el MENOR numero de 5 numeros
+
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "Ingrese su numero: ";
+    $numero = fgets(STDIN);
+    if ($mayor > $numero) {
+        $mayor = $numero;
+    }
+}
+echo "El menor numero es: " . $mayor;
+
 ?>
