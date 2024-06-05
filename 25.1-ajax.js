afd36f062a7c3fe60c5a2089336179d75df3a667
RogerJackD <1511638@senati.pe>
2024-06-05 17:51:11 -0500

    git push

--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -1,5 +1,4 @@
 function buscarPacientes() {
-    
     const $nombre = $("#nombre").val();
     //alert("Buscando..."+nombre);
     let datos ={
@@ -10,10 +9,40 @@ function buscarPacientes() {
         type : "post",
         data : datos,
         success : function(result) {
+            debugger;
             console.log(result);
+            const pacientes=$.parseJSON(result);
+            pacientes.forEach(item => {
+                agregarFilas("#tabla",item);
+            });
+
         }
     })
 
 
     return;
+}
+
+function eliminarpaciente(){
+    nost 
+}
+
+function agregarFilas(id,paciente) {
+    const html=
+    "<tr>"+
+    "<td>"+paciente.nombres+"</td>"+
+    "<td>"+paciente.edad+"</td>"+    
+    "<td>"+paciente.talla_m+"</td>"+
+    "<td>"+paciente.peso_kg+"</td>"+
+    "<td>"+paciente.sintoma_tos+"</td>"+
+    "<td>"+paciente.sintoma_fiebre+"</td>"+
+    "<td>"+paciente.sintoma_disnea+"</td>"+
+    "<td><button type='button' onclick=editar('"+paciente.nombres+"','"+paciente.edad+"');>Editar</button></td>"+
+    "</tr>";
+    $(id+" tr:last").after(html);
+}
+ 
+function editar(nombres,edad) {
+    $('#exampleModal').modal('show');    
+    $("#nombre2").val(nombres);
 }
--- 25Busquedapacientes.php
+++ 25Busquedapacientes.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Busqueda de Pacientessssss</title>
+    <title>Busqueda de Pacientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>    
-    <center><h1>Busqueda de Pacientesssss</h1></center>
+    <center><h1>Busqueda de Pacientes</h1></center>
     <!-- <form action="22.1-consulta-pdo.php" method="POST"> -->
     <input type="text" placeholder="Escribe aqui" name="nombre" id="nombre" /><br>
     <label id="tos" name="tos">Tos</label><input type="checkbox" for="tos">
     <button type="button" onclick="buscarPacientes();">Buscar</button>
-    <table style="border: 1px solid black;">
+    <table style="border: 1px solid black;" id="tabla">
         <tr>
             <td>Paciente</td>
             <td>Edad</td>
@@ -22,12 +22,63 @@
             <td>Fiebre</td>
             <td>Disnea</td>
             <td>Acciones</td>
-        </tr>
-           
+        </tr>          
     </table>
     <!-- </form> -->
-    <script type="text/javascript" 
+    <script type="text/javascript"
     src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="25.1-ajax.js"></script>
+ 
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Nuevo Paciente
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Formulario culeivin</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="name" class="form-label">Nombre:</label>
+                    <input type="text" class="form-control" id="nombre2" name="nombre2"/>
+
+
+                    <label for="name" class="form-label">edad:</label>
+                    <input type="text" class="form-control" id="nombre2" name="nombre2"/>
+
+                    <label for="name" class="form-label">talla:</label>
+                    <input type="text" class="form-control" id="nombre2" name="nombre2"/>
+
+                    <label for="name" class="form-label">peso:</label>
+                    <input type="text" class="form-control" id="nombre2" name="nombre2"/>
+
+                    <label id="tos" name="tos">Tos</label> <br>
+                    <input type="checkbox" for="tos"> <br>
+
+                    <label id="tos" name="tos">fiebre</label> <br>
+                    <input type="checkbox" for="tos"> <br>
+
+                    <label id="tos" name="tos">Disnea</label> <br>
+                    <input type="checkbox" for="tos"> <br>
+
+
+
+                    
+                    <button class="btn btn-primary">Guardar</button>
+                    <button class="btn btn-primary">Cancelar</button>
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
